d482a92d7fcee741f6c881fd98ccd2aa4fb41bb2
Xinrui Pan <R13129103348@outlook.com>
2026-01-12 18:02:13 +0800

    fix-调整图标颜色

--- src/components/icons/plusIcon.vue
+++ src/components/icons/plusIcon.vue
@@ -1,3 +1,3 @@
 <template>
-    <svg width="16" height="16" viewBox="0 0 48 48" fill="none"><path d="M5 24h38M24 5v38" stroke="#4E5969" stroke-width="4"/></svg>
+    <svg width="16" height="16" viewBox="0 0 48 48" fill="none"><path d="M5 24h38M24 5v38" stroke="#fff" stroke-width="4"/></svg>
 </template>
