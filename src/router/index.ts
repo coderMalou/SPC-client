7d37dfd88e6dc547bf6b4a03680be86456f13f59
Xinrui Pan <R13129103348@outlook.com>
2026-03-03 17:27:00 +0800

    update-路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { userStore } from '@/stores/user'
+import storage from '@/utils/storage'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,31 +8,57 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      redirect: '/login'
+      redirect: '/login',
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/login.vue')
+      component: () => import('@/views/login.vue'),
+      meta: {requireGuest: true},
     },
     {
       path: '/task',
       name: 'task',
       component: () => import('@/views/task/index.vue'),
-      children: [
-        {
-          path: '/task/detail',
-          name: 'taskDetail',
-          component: () => import('@/views/task/detail.vue'),
-        }
-      ]
+      meta: {requireAuth: true},
+      // children: [
+      //   {
+      //     path: '/task/detail',
+      //     name: 'taskDetail',
+      //     component: () => import('@/views/task/detail.vue'),
+      //     meta: {requireAuth: true},
+      //   }
+      // ]
     },
     {
       path: '/graph',
       name: 'graph',
       component: () => import('@/views/graph/index.vue'),
+      meta: {requireAuth: true},
     }
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const user = userStore()
+
+  if (to.meta.requireAuth) {
+    if (!user.isLoggedIn) {
+      const savedUser = storage.get('user_info', 'session');
+      if (savedUser) {
+        user.login(savedUser)
+        next()
+      } else {
+        next('/login')
+      }
+    } else {
+      next()
+    }
+  } else if (to.meta.requireGuest) {
+    if (!user.isLoggedIn) {
+      next()
+    } else next('/task')
+  } else next()
 })
 
 export default router
